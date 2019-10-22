e81f5f283811dcabcc61138556a315c97cc4ae06
Afifa-Aslam <afifaaslamsiddiqui@gmail.com>
2019-10-22 17:36:52 +0500

    updated

---
+++ Task8.1.py
@@ -0,0 +1,7 @@
+import random as r
+listOfWords = ['APPLE', 'BILBO', 'CHORUSED', 'DISIMAGINE', 'ENSURING', 'FORMALISING','GLITCHES', 'HARMINE', 'INDENTATION', 'JACKED', 'KALPACS', 'LAUNDRY', 'MASKED', 'NETTED', 
+'OXFORD', 'PARODY', 'QUOTIENTS', 'RACERS', 'SADNESS', 'THYREOID', 'UNDUE', 'VENT', 'WEDGED', 'XERIC', 'YOUTHHOOD', 'ZIFFS']
+string=""
+length=len(listOfWords)
+x=listOfWords[r.randint(0,length)]
+print(x)
